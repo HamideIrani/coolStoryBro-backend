1122d1f8e99c28b8818bb8410408db79608d78b1
HamideIrani <hamide.irani@gmail.com>
2022-06-23 23:38:24 +0200

    sorting stories

--- routers/spaces.js
+++ routers/spaces.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const Space = require("../models").space;
+const Story = require("../models").story;
 
 const router = new Router();
 
@@ -11,6 +12,27 @@ router.get("/", async (request, response) => {
     console.log(e.message);
     next(e);
   }
+});
+
+router.get("/:id", async (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+  if (isNaN(parseInt(id))) {
+    return response.status(400).send({ message: "Space id is not a number" });
+  }
+
+  const space = await Space.findByPk(id, {
+    include: [Story],
+    order: [[Story, "createdAt", "DESC"]],
+  });
+
+  if (space === null) {
+    return response.status(404).send({ message: "Space not found" });
+  }
+  console.log(space.stories);
+
+  response.status(200).send({ message: "ok", space });
 });
 
 module.exports = router;
