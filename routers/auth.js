a7ddcc7f9be69445b5df80c2d68ef229edafaaa4
HamideIrani <hamide.irani@gmail.com>
2022-06-23 23:59:36 +0200

    feature3-creating a space after sign up

--- routers/auth.js
+++ routers/auth.js
@@ -3,6 +3,7 @@ const { Router } = require("express");
 const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
+const Space = require("../models/").space;
 const { SALT_ROUNDS } = require("../config/constants");
 
 const router = new Router();
@@ -51,13 +52,22 @@ router.post("/signup", async (req, res) => {
 
     const token = toJWT({ userId: newUser.id });
 
-    res.status(201).json({ token, user: newUser.dataValues });
+    const newSpace = await Space.create({
+      title: `${newUser.name}'s space`,
+      description: null,
+    });
+
+    res
+      .status(201)
+      .json({ token, user: newUser.dataValues, space: newSpace.dataValues });
   } catch (error) {
     if (error.name === "SequelizeUniqueConstraintError") {
       return res
         .status(400)
         .send({ message: "There is an existing account with this email" });
     }
+
+    console.log(error);
 
     return res.status(400).send({ message: "Something went wrong, sorry" });
   }
