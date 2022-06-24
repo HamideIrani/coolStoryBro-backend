3d6436afdfbfe5de8979b326a0ee582983a92edb
HamideIrani <hamide.irani@gmail.com>
2022-06-24 12:33:05 +0200

    feature4-adding space to /me

--- routers/auth.js
+++ routers/auth.js
@@ -4,6 +4,7 @@ const { toJWT } = require("../auth/jwt");
 const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
 const Space = require("../models/").space;
+const Story = require("../models").story;
 const { SALT_ROUNDS } = require("../config/constants");
 
 const router = new Router();
@@ -55,6 +56,7 @@ router.post("/signup", async (req, res) => {
     const newSpace = await Space.create({
       title: `${newUser.name}'s space`,
       description: null,
+      userId: newUser.id,
     });
 
     res
@@ -77,9 +79,15 @@ router.post("/signup", async (req, res) => {
 // - get the users email & name using only their token
 // - checking if a token is (still) valid
 router.get("/me", authMiddleware, async (req, res) => {
+  const space = await Space.findOne({
+    where: { userId: req.user.id },
+    include: [Story],
+    order: [[Story, "createdAt", "Desc"]],
+  });
   // don't send back the password hash
   delete req.user.dataValues["password"];
-  res.status(200).send({ ...req.user.dataValues });
+
+  res.status(200).send({ ...req.user.dataValues, space });
 });
 
 module.exports = router;
